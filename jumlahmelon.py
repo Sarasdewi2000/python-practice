7658e7d8e7011a14cba5e53c2aa52fafc99cbe76
Saraswati Saraswati <94231953+Sarasdewi2000@users.noreply.github.com>
2022-05-05 16:04:15 +0700

    Create jumlahmelon.py

---
+++ jumlahmelon.py
@@ -0,0 +1,18 @@
+print("masukkan panjang: ")
+panjang = int(input())
+
+print("masukkan lebar: ")
+lebar = int(input())
+
+luas = panjang * lebar
+print(f"Maka luas adalah {panjang} * {lebar} = {luas}")
+jumlahmelon = luas * 2
+print(f"Jumlah melon adalah {jumlahmelon}")
+
+
+-------------------
+
+P = float(input("Masukkan panjang: "))
+L = float(input("Masukkan lebar: "))
+J = (P*L) * 2
+print ("Jumlah melon Pak Agus ada", J)
