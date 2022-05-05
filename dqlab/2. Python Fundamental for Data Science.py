72d5160e419dce4b9463af72e1b7098acca6a4bf
Saraswati Saraswati <94231953+Sarasdewi2000@users.noreply.github.com>
2022-05-05 22:56:32 +0700

    Update 2. Python Fundamental for Data Science.py

--- dqlab/2. Python Fundamental for Data Science.py
+++ dqlab/2. Python Fundamental for Data Science.py
@@ -173,3 +173,14 @@ pd.set_option("display.max_columns",50)
 table = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv")
 table.head()
 print(table)
+
+#pandas bar chart
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+table = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv")
+table.head()
+x_label = table ['NAMA KELURAHAN']
+plt.bar(x=np.arange(len(x_label)), height=table['LAKI-LAKI WNI'])
+plt.show()
