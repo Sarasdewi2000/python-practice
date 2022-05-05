3e9116cf7b1d22d8a646e0f95b8fb6effc06c20b
Saraswati Saraswati <94231953+Sarasdewi2000@users.noreply.github.com>
2022-05-05 22:58:10 +0700

    Create summary.py

---
+++ dqlab/summary.py
@@ -0,0 +1,23 @@
+#1. Introduction to Data Science with Python
+
+'''
+1. What is python?
+2. Why data scientist have to learn python?
+3. Python language structure (statement, variables, literals, operators, reserved words, whitespaces, comment)
+4. Type data
+5. Library
+'''
+
+#2. Python Fundamental for Data Science
+
+'''
+1. What is python and why python so populer?
+2. If statement (if, elif, else)
+3. Operation math
+4. Looping (lop and while)
+5. Functions (def, parameter, prosedur, return value)
+6. Module (import, alias, several functions, all functions)
+7. Read file csv
+8. Matplot lib
+
+'''
