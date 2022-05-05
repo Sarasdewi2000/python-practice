d6b1c54c424664922434fb9c0798953c0cad9059
Saraswati Saraswati <94231953+Sarasdewi2000@users.noreply.github.com>
2022-05-05 21:50:31 +0700

    Update and rename Introduction to Data Science with Python.py to 1. Introduction to Data Science with Python.py

---
+++ dqlab/1. Introduction to Data Science with Python.py
@@ -0,0 +1,33 @@
+#statement
+print("Hello World.")
+print("Saya Aksara, baru belajar Python.")
+#gapapa
+""" ini comment lho """
+
+#variabels
+bil1 = 10
+Bil_2 = 20
+Frasa = "Halo Dunia"
+bil1, Bil_2 = 10,20
+salam = "Selamat Pagi"; Penutup = "Salam Sejahtera"
+
+#comment
+#perintah pada baris ini tidak mempengaruhi program
+'''
+perintah ini tidak akan dieksekusi oleh Python
+dan perintah ini juga tidak akan dieksekusi
+perintah ini juga tidak akan dieksekusi
+'''
+print("jadi # digunakan untuk membuat comment pada Python")
+
+#typedata
+text = "Belajar Python di DQLab."
+print(list(text))
+print(tuple(text))
+print(set(text))
+
+#library
+import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
